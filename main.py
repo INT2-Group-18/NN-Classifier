93b0ab7158e5adfe02d443eba18d2587bf0f2a90
ionut <ionutmanasia1910@gmail.com>
2021-05-03 16:09:04 +0100

    changed cnn

--- main.py
+++ main.py
@@ -13,32 +13,46 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
 
-        self.conv_layer = nn.Sequential(
+        self.conv_layers = nn.Sequential(
 
-            # Conv Layer block 1
+            # Conv Layer 1
             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.05),
+
+            # Conv Layer 2
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.05),
             nn.MaxPool2d(kernel_size=2, stride=2),
 
-            # Conv Layer block 2
+            # Conv Layer 3
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.05),
+
+            # Conv Layer 4
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Dropout2d(p=0.05),
 
-            # Conv Layer block 3
+            # Conv Layer 5
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.05),
+
+            # Conv Layer 6
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout2d(p=0.05),
 
         )
 
@@ -56,7 +70,7 @@ class Net(nn.Module):
         """Perform forward."""
 
         # conv layers
-        x = self.conv_layer(x)
+        x = self.conv_layers(x)
 
         # flatten
         x = x.view(x.size(0), -1)
