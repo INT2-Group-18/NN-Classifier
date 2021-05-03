64072bb92dab62cd688f4bc0de5c28309cdc2adb
ionut <ionutmanasia1910@gmail.com>
2021-05-03 16:11:12 +0100

    more data augmentation

--- main.py
+++ main.py
@@ -88,6 +88,12 @@ def main():
         [transforms.ToTensor(),
          transforms.ColorJitter(),
          transforms.RandomCrop(32, padding=4),
+         transforms.RandomRotation(15),
+         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+         transforms.RandomHorizontalFlip()])
+
+    transformtest = transforms.Compose(
+        [transforms.ToTensor(),
          transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
          transforms.RandomHorizontalFlip()])
 
@@ -99,8 +105,8 @@ def main():
                                               shuffle=True, num_workers=3, pin_memory=True)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size*2,
+                                           download=True, transform=transformtest)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=3, pin_memory=True)
 
     classes = ('plane', 'car', 'bird', 'cat',
